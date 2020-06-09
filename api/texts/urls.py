53b0d6d38346cd20bc1fedc82a4a474da6c4cacf
Samuel Urias <msamuelurias@gmail.com>
2020-06-09 07:12:34 -0600

    Message post route

--- api/texts/urls.py
+++ api/texts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Credential
+from .views import Credential, Message
 
 urlpatterns = [
     path('credential/', Credential.as_view()),
+    path('message/', Message.as_view()),
 ]
--- api/texts/views.py
+++ api/texts/views.py
@@ -1,8 +1,9 @@
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Credentials
+from .models import Credentials, Messages
+from .utilities.authentication import authenticate as custom_auth
 
 
 # Create your views here.
@@ -15,7 +16,8 @@ class Credential(APIView):
 
         # Validate the request
         if key is None or shared_secret is None:
-            return Response({"message": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Invalid request"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         if Credentials.objects.filter(key=key).exists():
             return Response({}, status=status.HTTP_403_FORBIDDEN)
@@ -23,3 +25,24 @@ class Credential(APIView):
             new_credential = Credentials(key=key, shared_secret=shared_secret)
             new_credential.save()
             return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+class Message(APIView):
+
+    def post(self, request):
+        # Authentication
+        if not custom_auth(self.request.META, request.data):
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+        msg = request.data.get('msg', None)
+        tag = request.data.get('tag', None)
+
+        if msg is None or tag is None:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+        new_message = Messages(msg=msg, tag=tag)
+        new_message.save()
+
+        return Response({"id": new_message.pk}, status=status.HTTP_200_OK)
+
+        return Response({"message": "Everything is working as expected"})
