cb580d6c5390cd675f7fc035cde24aa472e0e587
Samuel Urias <msamuelurias@gmail.com>
2020-06-08 23:59:32 -0600

    Rename the field from 'shared_key' to 'shared_secret'

--- api/texts/models.py
+++ api/texts/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class Credentials(models.Model):
     key = models.TextField()
-    shared_key = models.TextField()
+    shared_secret = models.TextField()
 
     def __str__(self):
-        return ''.join([self.key, '|', self.shared_key])
+        return ''.join([self.key, '|', self.shared_secret])
 
 
 class Messages(models.Model):
