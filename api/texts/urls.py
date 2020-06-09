689955fdf29639aa89a431ddf829db123d21156c
Samuel Urias <msamuelurias@gmail.com>
2020-06-09 08:05:52 -0600

    It is possible to retrieve the messages by tag

--- api/texts/urls.py
+++ api/texts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, re_path
-from .views import Credential, Message
+from .views import Credential, Message, MessagesWithTag
 
 urlpatterns = [
     path('credential/', Credential.as_view()),
-    re_path(r'^message/(?P<id>\d+)*(/)*', Message.as_view())
-    # path('messages/<str:tag>', Message.as_view()),
+    re_path(r'^message/(?P<id>\d+)*(/)*', Message.as_view()),
+    path('messages/<str:tag>/', MessagesWithTag.as_view()),
 ]
