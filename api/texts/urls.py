8865e7f4cbcd70d4a5d86c9b316701669a1020ac
Samuel Urias <msamuelurias@gmail.com>
2020-06-09 07:36:24 -0600

    It is possible to retrieve existing messages

--- api/texts/urls.py
+++ api/texts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import Credential, Message
 
 urlpatterns = [
     path('credential/', Credential.as_view()),
-    path('message/', Message.as_view()),
+    re_path(r'^message/(?P<id>\d+)*(/)*', Message.as_view())
+    # path('messages/<str:tag>', Message.as_view()),
 ]
--- api/texts/views.py
+++ api/texts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -45,4 +46,16 @@ class Message(APIView):
 
         return Response({"id": new_message.pk}, status=status.HTTP_200_OK)
 
-        return Response({"message": "Everything is working as expected"})
+    def get(self, request, id=None):
+        if id is None:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        print("fid: ", id)
+        print(request.data)
+        message = get_object_or_404(Messages, pk=id)
+        print(message.msg)
+        return Response(
+            {"id": message.id, "msg": message.msg, "tag": message.tag},
+            status=status.HTTP_200_OK
+        )
